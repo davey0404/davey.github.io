3c0d2b55c17df97533609b67b602c015833962d6
daveyrpt <89343504+daveyrpt@users.noreply.github.com>
2022-05-23 02:59:27 +0800

    upload new content

---
+++ assets/js/slider.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        991: {
+            slidesPerView: 1,
+        },
+        1200: {
+            slidesPerView: 1,
+        },
+    },
+});
+//console.log('test');
